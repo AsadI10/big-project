d0060ca33cee328c28d0d5580b117cc2a4f857c8
Asadi <imtiazasad74@gmail.com>
2024-01-23 20:15:05 +0000

    initial commit

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+        <h1>Navigation</h1>
+        <h2>Menu</h2>
+    </div>
+  )
+}
+
+export default Nav
---
+++ src/components/SideBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SideBar = () => {
+  return (
+    <div>
+      <h1>Meron is great teacher </h1>
+    </div>
+  )
+}
+
+export default SideBar
