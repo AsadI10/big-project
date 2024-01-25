d1718ec81eb92ced4229b7a7ed56a6f4643491e8
Asadi <imtiazasad74@gmail.com>
2024-01-25 21:26:21 +0000

    my second commit

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SideBar = () => {
   return (
     <div>
-      <h1>Meron is great teacher </h1>
+
     </div>
   )
 }
---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./NavBar.css";
+import Nav from 'react-bootstrap/Nav';
+
+function NavBar() {
+  return (
+    <div className='nav'>
+    <Nav variant="tabs" defaultActiveKey="/home">
+      <Nav.Item>
+        <Nav.Link href="/home">Home</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-1">Contact Us</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-2">Plan your Visit</Nav.Link>
+      </Nav.Item>
+    </Nav>
+    </div>
+  );
+}
+
+export default NavBar;
