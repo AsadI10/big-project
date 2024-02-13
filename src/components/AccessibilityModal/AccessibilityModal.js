7f93aaad7bcef2e074eaa1aec78e9a8090ed00e8
Asadi <imtiazasad74@gmail.com>
2024-02-13 12:09:10 +0000

    added accessibilitymodal

---
+++ src/components/AccessibilityModal/AccessibilityModal.js
@@ -0,0 +1,20 @@
+// AccessibilityModal.js
+import React from 'react';
+import Modal from 'react-bootstrap/Modal'; //using react-bootstrap for modal functionality
+import './AccessibilityModal.css';
+
+const AccessibilityModal = ({ show, onHide }) => {
+  return (
+    <Modal show={show} onHide={onHide} centered>
+      <Modal.Header closeButton>
+        <Modal.Title>Accessibility Adjustments</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {/* The body of the modal with accessibility options */}
+        {/* Implement your accessibility features here */}
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default AccessibilityModal;
