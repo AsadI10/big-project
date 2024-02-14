b0daa743ffee6378d90a13bc2cdb5b15ef81c1ed
Asadi <imtiazasad74@gmail.com>
2024-02-14 23:46:31 +0000

    added dark contrast

--- src/components/AccessibilityModal/AccessibilityModal.js
+++ src/components/AccessibilityModal/AccessibilityModal.js
@@ -1,20 +1,30 @@
-import { useContext } from 'react';
+// import { useContext } from 'react';
 import React from 'react';
 import Modal from 'react-bootstrap/Modal'; //using react-bootstrap for modal functionality
 import './AccessibilityModal.css';
-import { ThemeContext } from '../contexts/ThemeContext.js'
+// import { ThemeContext } from '../contexts/ThemeContext.js'
+import useLocalStorage from 'use-local-storage';
 
 const AccessibilityModal = ({ show, onHide }) => {
-  const { isDarkThemeEnabled, toggleDarkTheme } = useContext(ThemeContext);
+  // const { isDarkThemeEnabled, toggleDarkTheme } = useContext(ThemeContext);
+  const [theme, setTheme] = useLocalStorage('theme' ? 'dark' : 'light')
+  const switchTheme = () => {
+      const newTheme = theme === 'light' ? 'dark' : 'light';
+      setTheme(newTheme);
+    }
   return (
     <Modal show={show} onHide={onHide} centered>
       <Modal.Header closeButton>
         <Modal.Title className='modal-title-custom'>Accessibility Adjustments</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-      <button className='button-square' onClick={toggleDarkTheme}>
+      {/* <button className='button-square' onClick={toggleDarkTheme}>
           {isDarkThemeEnabled ? 'Disable Dark Theme' : 'Enable Dark Theme'}
-        </button>
+        </button> */}
+    <div className='theme-Toggle' data-theme={theme} >
+        {/* <button>Light Theme</button> */}
+        <i onClick={switchTheme} className='fas fa-toggle-on'>Light Theme</i>
+    </div>
       </Modal.Body>
     </Modal>
   );
