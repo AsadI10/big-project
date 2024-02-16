f58ffa3a741e5c8f20ea742751bbda2faa000498
Asadi <imtiazasad74@gmail.com>
2024-02-16 22:35:22 +0000

    added a news section to the body

---
+++ src/components/body/LatestNews.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const newsItems = [
+    {
+        title: "Get ready for the biggest year in Vista Voyage History",
+        image: "",
+        date: "READ MORE"
+    },
+    {
+        
+    }
+]
+
+const LatestNews = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LatestNews
