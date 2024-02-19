d8c6449e88f4baad77756f63d349230ec32dea98
Asadi <imtiazasad74@gmail.com>
2024-02-19 21:51:47 +0000

    added footer to the app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SideBar from './components/SideBar.js';
 import HomePage from './components/homepage/HomePage.js';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Body from '../src/components/body/Body.js';
-import Footer from './Practice-Components/footer/Footer.js';
+import Footer from './components/footer/Footer.js';
 import LatestNews from './components/body/LatestNews.js';
 
 function App() {
