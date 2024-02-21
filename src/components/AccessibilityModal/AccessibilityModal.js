287ebe1c08a6cd5ca9d6d7e6bd96d26a73cca5e3
Asadi <imtiazasad74@gmail.com>
2024-02-21 11:16:31 +0000

    added adjusttextcolor

--- src/components/AccessibilityModal/AccessibilityModal.js
+++ src/components/AccessibilityModal/AccessibilityModal.js
@@ -17,7 +17,6 @@ const toggleLowSaturation = () => {
   setIsLowSaturation(shouldBeLowSaturation);
   document.body.classList.toggle('low-saturation', shouldBeLowSaturation);
 };
-
 useEffect(() => {
   // The cleanup function runs when the `isLowSaturation` state changes
   // or when the component is unmounted.
@@ -26,10 +25,22 @@ useEffect(() => {
   };
 }, [isLowSaturation]);
 
+//Implementing adjust text colors
+const textColors = ['#34568B', '#6B5B95', '#88B04B', '#F7CAC9', '#92A8D1', '#955251', '#B565A7'];
+const [textColor, setTextColor] = useState('#000000'); // Default text color
+const changeTextColor = (color) => {
+  setTextColor(color);
+  document.body.style.color = color; // This will change the text color of the entire body
+};
+useEffect(() => {
+  document.documentElement.style.setProperty('--text-color', textColor);
+}, [textColor]);
+
+
 // Define the available background colors
 const backgroundColors = ['#FFFFFF', '#0000FF', '#800080', '#FF0000', '#00FF00', '#FFFF00', '#000000'];
 
-     // State to hold the selected background color
+// State to hold the selected background color
 const [backgroundColor, setBackgroundColor] = useState('#FFFFFF');
 
 useEffect(() => {
@@ -87,6 +98,28 @@ return (
       <button onClick={toggleLowSaturation} className="square-button">
           {isLowSaturation ? 'Normal Saturation' : 'Low Saturation'}
       </button>
+<div className="text-color-selector-container">
+  <p>Adjust Text Colors</p>
+  <div className="color-options">
+    {textColors.map((color, index) => (
+      <button
+        key={index}
+        style={{
+          backgroundColor: color,
+          width: '30px',
+          height: '30px',
+          borderRadius: '50%',
+          border: 'none',
+          cursor: 'pointer',
+          margin: '0 5px',
+        }}
+        onClick={() => changeTextColor(color)}
+        aria-label={`Change text color to ${color}`}
+      />
+    ))}
+  </div>
+  <button className='adjust-color' onClick={onHide}>Cancel</button>
+</div>
       </Modal.Body>
     </Modal>
   );
